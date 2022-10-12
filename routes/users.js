999e761fd5d8195360cb8aa463761436b11e7f92
Zeynep Baydemir <112198215+zeynep-baydemir@users.noreply.github.com>
2022-10-12 16:41:44 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const omit = require('just-omit');
 
 router.get('/', async (req,res) => {
     try {
